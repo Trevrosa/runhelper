0b0351e0836a34f4b8521fbdf7bab54befacccd9
Trevor Chan <trev@trevrosa.dev>
2025-08-18 20:07:32 +0800

    helper: improve logging

--- helper/src/tasks.rs
+++ helper/src/tasks.rs
@@ -1,6 +1,7 @@
 use std::time::Duration;
 
-use reqwest_websocket::{Bytes, Message, RequestBuilderExt};
+use reqwest_websocket as reqwest_ws;
+use reqwest_ws::{Bytes, Message, RequestBuilderExt};
 use rocket::{
     futures::StreamExt,
     tokio::{self, sync::broadcast::Sender},
@@ -8,31 +9,33 @@ use rocket::{
 
 use crate::{RUNNER_ADDR, UrlExt};
 
+pub async fn websocket(
+    client: &reqwest::Client,
+    url: impl reqwest::IntoUrl,
+) -> Result<reqwest_ws::WebSocket, reqwest_ws::Error> {
+    client
+        .get(url)
+        .upgrade()
+        .send()
+        .await?
+        .into_websocket()
+        .await
+}
+
 const WS_TIMEOUT: Duration = Duration::from_secs(2);
 
 /// transmits the stats from the runner to a channel.
+#[tracing::instrument(skip_all)]
 pub async fn stats_helper(client: reqwest::Client, tx: Sender<Bytes>) {
     loop {
-        let resp = client
-            .get(RUNNER_ADDR.join_unchecked("stats"))
-            .timeout(Duration::from_secs(4))
-            .upgrade()
-            .send()
-            .await;
-        let Ok(resp) = resp else {
-            tracing::trace!("failed to send websocket request, reconnecting in {WS_TIMEOUT:?}");
-            tokio::time::sleep(WS_TIMEOUT).await;
-            continue;
-        };
-
-        let runner_ws = resp.into_websocket().await;
+        let runner_ws = websocket(&client, RUNNER_ADDR.join_unchecked("stats")).await;
         let Ok(mut runner_ws) = runner_ws else {
-            tracing::trace!("failed to upgrade to websocket, reconnecting in {WS_TIMEOUT:?}");
+            tracing::trace!("failed to connect, waiting {WS_TIMEOUT:?}..");
             tokio::time::sleep(WS_TIMEOUT).await;
             continue;
         };
 
-        tracing::info!("connected to stats websocket");
+        tracing::info!("connected");
 
         while let Some(message) = runner_ws.next().await {
             let message = match message {
@@ -52,34 +55,23 @@ pub async fn stats_helper(client: reqwest::Client, tx: Sender<Bytes>) {
             }
         }
 
-        tracing::warn!("runner websocket closed, reconnecting in {WS_TIMEOUT:?}");
+        tracing::warn!("disconnected, waiting {WS_TIMEOUT:?}..");
         tokio::time::sleep(WS_TIMEOUT).await;
     }
 }
 
 /// transmits the stats from the runner to a channel.
+#[tracing::instrument(skip_all)]
 pub async fn console_helper(client: reqwest::Client, tx: Sender<String>) {
     loop {
-        let resp = client
-            .get(RUNNER_ADDR.join_unchecked("console"))
-            .timeout(Duration::from_secs(4))
-            .upgrade()
-            .send()
-            .await;
-        let Ok(resp) = resp else {
-            tracing::trace!("failed to send websocket request, reconnecting in {WS_TIMEOUT:?}");
-            tokio::time::sleep(WS_TIMEOUT).await;
-            continue;
-        };
-
-        let runner_ws = resp.into_websocket().await;
+        let runner_ws = websocket(&client, RUNNER_ADDR.join_unchecked("console")).await;
         let Ok(mut runner_ws) = runner_ws else {
-            tracing::trace!("failed to upgrade to websocket, reconnecting in {WS_TIMEOUT:?}");
+            tracing::trace!("failed to connect, waiting {WS_TIMEOUT:?}..");
             tokio::time::sleep(WS_TIMEOUT).await;
             continue;
         };
 
-        tracing::info!("connected to console websocket");
+        tracing::info!("connected");
 
         while let Some(message) = runner_ws.next().await {
             let message = match message {
@@ -99,7 +91,7 @@ pub async fn console_helper(client: reqwest::Client, tx: Sender<String>) {
             }
         }
 
-        tracing::warn!("runner websocket closed, reconnecting in {WS_TIMEOUT:?}");
+        tracing::warn!("disconnected, waiting {WS_TIMEOUT:?}..");
         tokio::time::sleep(WS_TIMEOUT).await;
     }
 }
