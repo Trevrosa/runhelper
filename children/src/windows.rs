f90c22d3eeebcedacfc72e6ace06ca3a004cc7b2
trev <trev@trevrosa.dev>
2025-12-13 14:01:51 +0800

    children: cleanup code

--- children/src/windows.rs
+++ children/src/windows.rs
@@ -10,11 +10,6 @@ use windows::Win32::{
 
 use crate::ProcessInfo;
 
-pub(crate) fn process_filter(process: &ProcessInfo) -> bool {
-    // is the system idle process
-    process.parent_pid == process.pid
-}
-
 pub(crate) fn get_processes() -> anyhow::Result<Vec<ProcessInfo>> {
     let mut processes = Vec::new();
 
@@ -23,8 +18,8 @@ pub(crate) fn get_processes() -> anyhow::Result<Vec<ProcessInfo>> {
     unsafe {
         let snapshot_handle = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0)?;
         {
-            // do NOT return early from this block
-            let mut process_entry = std::mem::zeroed::<PROCESSENTRY32>();
+            // do NOT return early from this block; `CloseHandle` must be run after we are done.
+            let mut process_entry: PROCESSENTRY32 = std::mem::zeroed();
             let process_entry_size = u32::try_from(std::mem::size_of::<PROCESSENTRY32>());
             match process_entry_size {
                 Ok(process_entry_size) => {
@@ -32,7 +27,9 @@ pub(crate) fn get_processes() -> anyhow::Result<Vec<ProcessInfo>> {
                     match Process32First(snapshot_handle, &raw mut process_entry) {
                         Ok(()) => loop {
                             processes.push(ProcessInfo {
-                                name: CStr::from_ptr(process_entry.szExeFile.as_ptr()).to_string_lossy().into_owned(),
+                                name: CStr::from_ptr(process_entry.szExeFile.as_ptr())
+                                    .to_string_lossy()
+                                    .into_owned(),
                                 pid: process_entry.th32ProcessID,
                                 parent_pid: process_entry.th32ParentProcessID,
                             });
@@ -52,7 +49,7 @@ pub(crate) fn get_processes() -> anyhow::Result<Vec<ProcessInfo>> {
                     }
                 }
                 Err(e) => {
-                    error = Some(anyhow!("couldn't cast process entry to u32: {e}"));
+                    error = Some(anyhow!("couldn't cast process entry size to u32: {e}"));
                 }
             }
         }
@@ -60,7 +57,7 @@ pub(crate) fn get_processes() -> anyhow::Result<Vec<ProcessInfo>> {
     }
 
     if let Some(error) = error {
-        return Err(error)
+        return Err(error);
     }
 
     Ok(processes)
